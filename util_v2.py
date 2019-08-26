ca69008cfaf638d8064dc6a5b3889838dbf0317f
Vahid Khorasani Ghassab <42419596+s172444s@users.noreply.github.com>
2019-08-26 11:17:34 -0400

    Update util_v2.py

--- util_v2.py
+++ util_v2.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import cv2 as cv
 import numpy as np
 from pil import Image
@@ -7,6 +8,53 @@ from Network import UNet
 import argparse
 from utils import resize_and_crop, normalize, split_img_into_squares, hwc_to_chw, merge_masks
 from torchvision import transforms
+#import cv2
+
+MAX_FEATURES = 500
+GOOD_MATCH_PERCENT = 0.15
+
+
+def alignImages(im1, im2):
+    # Convert images to grayscale
+    im1Gray = cv.cvtColor(im1, cv.COLOR_BGR2GRAY)
+    im2Gray = cv.cvtColor(im2, cv.COLOR_BGR2GRAY)
+
+    # Detect ORB features and compute descriptors.
+    orb = cv.ORB_create(MAX_FEATURES)
+    keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
+    keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
+
+    # Match features.
+    matcher = cv.DescriptorMatcher_create(cv.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+    matches = matcher.match(descriptors1, descriptors2, None)
+
+    # Sort matches by score
+    matches.sort(key=lambda x: x.distance, reverse=False)
+
+    # Remove not so good matches
+    numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
+    matches = matches[:numGoodMatches]
+
+    # Draw top matches
+    #imMatches = cv.drawMatches(im1, keypoints1, im2, keypoints2, matches, None)
+    #cv.imwrite("matches.jpg", imMatches)
+
+    # Extract location of good matches
+    points1 = np.zeros((len(matches), 2), dtype=np.float32)
+    points2 = np.zeros((len(matches), 2), dtype=np.float32)
+
+    for i, match in enumerate(matches):
+        points1[i, :] = keypoints1[match.queryIdx].pt
+        points2[i, :] = keypoints2[match.trainIdx].pt
+        #print(points1[i][0] - points2[i][0])
+    # Find homography
+    h, mask = cv.findHomography(points1, points2, cv.RANSAC)
+
+    # Use homography
+    height, width, channels = im2.shape
+    im1Reg = cv.warpPerspective(im1, h, (width, height))
+
+    return points1, points2
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -459,3 +507,5 @@ def static_point_detector():
     cap.release()
     cv.destroyAllWindows()
     return point
+
+
