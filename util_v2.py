564bc6e3faa7d78b24b56eed50c4940dbb3f3928
Vahid Khorasani Ghassab <42419596+s172444s@users.noreply.github.com>
2019-08-30 16:08:58 -0400

    Update util_v2.py

--- util_v2.py
+++ util_v2.py
@@ -8,10 +8,67 @@ from Network import UNet
 import argparse
 from utils import resize_and_crop, normalize, split_img_into_squares, hwc_to_chw, merge_masks
 from torchvision import transforms
+import cv2
 #import cv2
 
 MAX_FEATURES = 500
 GOOD_MATCH_PERCENT = 0.15
+
+def static_point_detecor_homography(img1, img2):
+    #point = []
+    im1 = cv2.imread(img1, cv2.IMREAD_COLOR)
+    im2 = cv2.imread(img2, cv2.IMREAD_COLOR)
+    # Convert images to grayscale
+    im1Gray = cv.cvtColor(im1, cv.COLOR_BGR2GRAY)
+    im2Gray = cv.cvtColor(im2, cv.COLOR_BGR2GRAY)
+
+    # Detect ORB features and compute descriptors.
+    orb = cv.ORB_create(MAX_FEATURES)
+    keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
+    keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
+
+    # Match features.
+    matcher = cv.DescriptorMatcher_create(cv.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+    matches = matcher.match(descriptors1, descriptors2, None)
+
+    # Sort matches by score
+    matches.sort(key=lambda x: x.distance, reverse=False)
+
+    # Remove not so good matches
+    numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
+    matches = matches[:numGoodMatches]
+
+    # Draw top matches
+    #imMatches = cv.drawMatches(im1, keypoints1, im2, keypoints2, matches, None)
+    #cv.imwrite("matches.jpg", imMatches)
+
+    # Extract location of good matches
+    points1 = np.zeros((len(matches), 2), dtype=np.float32)
+    points2 = np.zeros((len(matches), 2), dtype=np.float32)
+
+    for i, match in enumerate(matches):
+        points1[i, :] = keypoints1[match.queryIdx].pt
+        points2[i, :] = keypoints2[match.trainIdx].pt
+        #print(points1[i][0] - points2[i][0])
+    # Find homography
+    matrix, mask = cv.findHomography(points1, points2, cv.RANSAC)
+    A = np.array(matrix)
+    #B = np.array([[points1[0][0],points1[0][1],1]]).transpose()
+    B = np.array([[647, 553, 1]]).transpose()
+    C = A.dot(B)
+    # print(C)
+    # print(C[0][0], C[1][0])
+    # exit(0)
+    #point.append(C[0][0])
+    # print(points2)
+    # print(C)
+    # exit(0)
+
+    # Use homography
+    height, width, channels = im2.shape
+    im1Reg = cv.warpPerspective(im1, matrix, (width, height))
+
+    return C[0][0]/C[2][0], C[1][0]/C[2][0]
 
 
 def alignImages(im1, im2):
@@ -48,11 +105,17 @@ def alignImages(im1, im2):
         points2[i, :] = keypoints2[match.trainIdx].pt
         #print(points1[i][0] - points2[i][0])
     # Find homography
-    h, mask = cv.findHomography(points1, points2, cv.RANSAC)
+    matrix, mask = cv.findHomography(points1, points2, cv.RANSAC)
+    # A = np.array(matrix)
+    # B = np.array([[points1[0][0],points1[0][1],1]]).transpose()
+    # C = A.dot(B)
+    # print(points2)
+    # print(C)
+    # exit(0)
 
     # Use homography
     height, width, channels = im2.shape
-    im1Reg = cv.warpPerspective(im1, h, (width, height))
+    im1Reg = cv.warpPerspective(im1, matrix, (width, height))
 
     return points1, points2
 
